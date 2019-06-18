2cc0bc977d17865926378b1fb872cbceb18f53a5
riccardobrs <riccardo158b@gmail.com>
2019-06-18 15:21:19 +0200

    efficiencies

---
+++ eff.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main (int argc, char ** argv) {
+    
+    double A = 6.67E+04;
+    double A_err = 100;
+    double BR = 0.905;
+    double r_coinc = 5.20829;
+    double r_coinc_err = 0.00666156;
+    double r_1 = 28.9165;
+    double r_1_err = 0.145727;
+    double r_2 = 57.5277;
+    double r_2_err = 0.201624;
+    double eff_1 = r_coinc/r_2;
+    double eff_2 = r_coinc/r_1;
+    double eff_1_err = sqrt(pow((r_coinc_err/r_2),2)+pow((r_coinc*r_2_err*r_2_err/(r_2*r_2)),2));
+    double eff_2_err = sqrt(pow((r_coinc_err/r_1),2)+pow((r_coinc*r_1_err*r_1_err/(r_1*r_1)),2));
+    double num = r_1*r_2;
+    double num_err = sqrt( pow((r_1*r_2_err),2)+pow((r_2*r_1_err),2));
+    double den = 2*A*BR*r_coinc;
+    double den_err = 2*BR*sqrt(pow((A*r_coinc_err),2)+pow((r_coinc*A_err),2));
+    double ang_solido_1 = num/den;
+    double ang_solido_1_err = sqrt(pow((num_err/den),2)+pow((num*den_err/(den*den)),2));
+    
+    cout << "(Angolo solido 1pol / 4pi) = " << ang_solido_1 << " +- " << ang_solido_1_err << endl;
+    
+    return 0;
+
+}
